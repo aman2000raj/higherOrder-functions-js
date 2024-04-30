3e958aeffd9a2522930dd2b24beaa8ee3b63f05c
Aman Raj <66414882+aman2000raj@users.noreply.github.com>
2024-04-30 15:28:33 +0530

    Add index file with all method imported

---
+++ index.js
@@ -0,0 +1,47 @@
+import map from './map.js';
+import forEach from './forEach.js';
+import filter from './filter.js';
+import reduce from './reduce.js';
+
+const numbers = [1, 2, 3, 4, 5];
+const fruits = ['apple', 'orange', 'banana', 'cherry'];
+const scores = [70, 85, 90, 50, 65];
+//
+// double numbers using custom map methods
+const callBackFunctionForMap = (Element, idx, array) => Element * 2;
+const doubledNumbers = map(numbers, callBackFunctionForMap);
+
+console.log(doubledNumbers); // Output: [1, 4, 6, 8, 10]
+//
+
+// logging fruits with indexes using custom forEach method
+const callBackFunctionForEach = (Element, idx, array) => {
+  console.log(`Fruits ${idx + 1}: ${Element}`);
+};
+forEach(fruits, callBackFunctionForEach);
+// Output:
+// Fruits 1: apple
+// Fruits 2: orange
+// Fruits 3: banana
+// Fruits 4: cherry
+//
+
+// filter scores array with custom filter methods
+const callBackFuctionForFilter = (Element, idx, array) => {
+  // scores above 70 is considered.
+  if (Element >= 70) return true;
+  return false;
+};
+
+const filteredScores = filter(scores, callBackFuctionForFilter);
+console.log(filteredScores);
+// Output: [70, 85, 90]
+//
+
+// calculate total of number array using custom reduce method
+const callBackFuctionForReduce = (accumulator = 0, Element, idx, array) =>
+  accumulator + Element;
+
+const totalSum = reduce(numbers, callBackFuctionForReduce);
+console.log(totalSum);
+// Output: 15
